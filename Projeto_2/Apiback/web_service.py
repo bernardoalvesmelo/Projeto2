4f3af1ca55ae78ba47ebaa1916107f4b163a7941
bernardoalvesmelo <106593708+bernardoalvesmelo@users.noreply.github.com>
2023-03-12 19:59:12 -0300

    feat: adição básica de autenticação

--- Projeto_2/Apiback/web_service.py
+++ Projeto_2/Apiback/web_service.py
@@ -1,116 +1,177 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from markupsafe import escape
-import psycopg2
 from DB_Controller import DBController
+import hashlib
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 db_controller = DBController()
 if db_controller.create_db("equipamentos_bd"):
     db_controller.create_tables()
 
+#!!!Change the secret_key and passwords, the default ones are public as the code is public on git!!!
+#!!!Mude o secret_key e passwords, as de padrão estão públicas no git!!!
+app.secret_key = '_5#y3L"F4Q8z\n\xec]/'
+app.config['SESSION_COOKIE_HTTPONLY'] = False
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+
+users = {
+    "admin": {"username": "admin", "password": "admin"},
+    "user": {"username": "user", "password": "password"}
+}
+
+for user in users:
+    password = users[user]["password"]
+    users[user]["password"] = hashlib.sha256(password.encode()).hexdigest()
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = users.get(username)
+    if user and hashlib.sha256(password.encode()).hexdigest() == user["password"]:
+        session["username"] = username
+        return jsonify("Login sucedido!")
+    else:
+        return jsonify("Erro no usário ou senha!")
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return jsonify("Logout sucedido")
 
 @app.route('/equipamentos', methods=['GET'])
 def EQUIP():
-    equip = db_controller.return_equipamento_list()
-    return jsonify(equip)
+    if "username" in session:
+        equip = db_controller.return_equipamento_list()
+        return jsonify(equip)
+    else:
+        return jsonify("Necessário login")
 
 
 @app.route('/equipamentos/<id>', methods=['GET'])
 def EQUIPALL(id):
-    if not id.isdigit():
-        return jsonify("Header inválido")
-    id = int(id)
-    equip = db_controller.return_equipamento_obj(id)
-    return jsonify(equip)
+    if "username" in session:
+        if not id.isdigit():
+            return jsonify("Header inválido")
+        id = int(id)
+        equip = db_controller.return_equipamento_obj(id)
+        return jsonify(equip)
+    else:
+        return jsonify("Necessário login")
 
 
 @app.route('/equipamentos/update', methods=['POST'])
 def EQUIPUPDATE():
-    data = request.get_json()
-    if not isinstance(data, dict):
-        return jsonify("Body inválido")
-    update = purge(data)
-    if db_controller.update_equipamento(update):
-        return jsonify("Update completado")
+    if "username" in session:
+        data = request.get_json()
+        if not isinstance(data, dict):
+            return jsonify("Body inválido")
+        update = purge(data)
+        if db_controller.update_equipamento(update):
+            return jsonify("Update completado")
+        else:
+            return jsonify("Update não completado")
     else:
-        return jsonify("Update não completado")
+        return jsonify("Necessário login")
 
 
 @app.route('/equipamentos/insert', methods=['POST'])
 def EQUIPINSERT():
-    data = request.get_json()
-    if not isinstance(data, dict):
-        return jsonify("Body inválido")
-    insert = purge(data)
-    if db_controller.insert_equipamento(insert):
-        return jsonify("Insert completado")
+    if "username" in session:
+        data = request.get_json()
+        if not isinstance(data, dict):
+            return jsonify("Body inválido")
+        insert = purge(data)
+        if db_controller.insert_equipamento(insert):
+            return jsonify("Insert completado")
+        else:
+            return jsonify("Insert não completado")
     else:
-        return jsonify("Insert não completado")
+        return jsonify("Necessário login")
 
 
 @app.route('/equipamentos/remove/<id>', methods=['GET'])
 def EQUIPREMOVE(id):
-    if not id.isdigit():
-        return jsonify("Header inválido")
-    id = int(id)
-    if db_controller.remove_equipamento(id):
-        return jsonify("Equipamento removido")
+    if "username" in session:
+        if not id.isdigit():
+            return jsonify("Header inválido")
+        id = int(id)
+        if db_controller.remove_equipamento(id):
+            return jsonify("Equipamento removido")
+        else:
+            return jsonify("Equipamento não removido")
     else:
-        return jsonify("Equipamento não removido")
+        return jsonify("Necessário login")
 
 # chamados
 
 
 @app.route('/chamados', methods=['GET'])
 def CHAM():
-    cham = db_controller.return_chamado_list()
-    return jsonify(cham)
+    if "username" in session:
+        cham = db_controller.return_chamado_list()
+        return jsonify(cham)
+    else:
+        return jsonify("Necessário login")
 
 
 @app.route('/chamados/<id>', methods=['GET'])
 def CHAMALL(id):
-    if not id.isdigit():
-        return jsonify("Header inválido")
-    id = int(id)
-    cham = db_controller.return_chamado_obj(id)
-    return jsonify(cham)
+    if "username" in session:
+        if not id.isdigit():
+            return jsonify("Header inválido")
+        id = int(id)
+        cham = db_controller.return_chamado_obj(id)
+        return jsonify(cham)
+    else:
+        return jsonify("Necessário login")
 
 
 @app.route('/chamados/update', methods=['POST'])
 def CHAMUPDATE():
-    data = request.get_json()
-    if not isinstance(data, dict):
-        return jsonify("Body inválido")
-    update = purge(data)
-    if db_controller.update_chamado(update):
-        return jsonify("Update completado")
+    if "username" in session:
+        data = request.get_json()
+        if not isinstance(data, dict):
+            return jsonify("Body inválido")
+        update = purge(data)
+        if db_controller.update_chamado(update):
+            return jsonify("Update completado")
+        else:
+            return jsonify("Update não completado")
     else:
-        return jsonify("Update não completado")
+        return jsonify("Necessário login")
 
 
 @app.route('/chamados/insert', methods=['POST'])
 def CHAMINSERT():
-    data = request.get_json()
-    if not isinstance(data, dict):
-        return jsonify("Body inválido")
-    insert = purge(data)
-    if db_controller.insert_chamado(insert):
-        return jsonify("Insert completado")
+    if "username" in session:
+        data = request.get_json()
+        if not isinstance(data, dict):
+            return jsonify("Body inválido")
+        insert = purge(data)
+        if db_controller.insert_chamado(insert):
+            return jsonify("Insert completado")
+        else:
+            return jsonify("Insert não completado")
     else:
-        return jsonify("Insert não completado")
+        return jsonify("Necessário login")
 
 
 @app.route('/chamados/remove/<id>', methods=['GET'])
 def CHAMREMOVE(id):
-    if not id.isdigit():
-        return jsonify("Header inválido")
-    id = int(id)
-    if db_controller.remove_chamado(id):
-        return jsonify("Chamado removido")
+    if "username" in session:
+        if not id.isdigit():
+            return jsonify("Header inválido")
+        id = int(id)
+        if db_controller.remove_chamado(id):
+            return jsonify("Chamado removido")
+        else:
+            return jsonify("Chamado não removido")
     else:
-        return jsonify("Chamado não removido")
+        return jsonify("Necessário login")
 
 
 def purge(data):
